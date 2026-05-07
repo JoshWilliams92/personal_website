0f416c96982076b83feb09bf2f7a221d5115bcee
Josh <joshfw27492@gmail.com>
2026-05-07 15:54:49 +0100

    'analytics'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import { type Metadata } from 'next'
 
+
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
+import { Analytics } from '@vercel/analytics/next'
 
 import '@/styles/tailwind.css'
 
@@ -33,6 +35,7 @@ export default function RootLayout({
             <Layout>{children}</Layout>
           </div>
         </Providers>
+        <Analytics />
       </body>
     </html>
   )
